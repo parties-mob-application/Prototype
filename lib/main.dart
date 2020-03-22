c8d706712f2eef6f537b72561e17a0b3692d98a8
Sam Lightowler <slightowler7@gmail.com>
2020-03-22 17:16:19 +0000

    initial layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,85 +1,54 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(Home());
 
-class MyApp extends StatelessWidget {
+
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-    Widget titleSection = Container(
-        padding: const EdgeInsets.all(32),
-        child: Text ('Parties',
-          style: TextStyle (
-            fontSize: 72,
-            fontWeight: FontWeight.bold,
-          ),
-        )
-    );
-
-    Widget dashboardSection = Container(
-        padding: const EdgeInsets.all(64),
-        decoration: BoxDecoration(
-          border: Border.all(
-            color: Color(0xff000000),
-            width: 4
-          )
-        ),
-        child: Text ('Dashboard',
-          style: TextStyle (
-            fontSize: 36,
-            fontWeight: FontWeight.bold,
-          ),
-        )
-    );
-
-    Widget partiesSection = Container(
-        padding: const EdgeInsets.all(64),
-        decoration: BoxDecoration(
-          border: Border.all(
-            color: Color(0xff000000),
-            width: 4
-          )
-        ),
-        child: Text ('People',
-          style: TextStyle (
-            fontSize: 36,
-            fontWeight: FontWeight.bold,
-          ),
-        )
-    );
-
-    Widget adSpaceSection = Container(
-        padding: const EdgeInsets.all(32),
-        decoration: BoxDecoration(
-          border: Border.all(
-            color: Color(0xff000000),
-            width: 4
-          )
-        ),
-        child: Text ('ad space',
-          style: TextStyle (
-            fontSize: 36,
-            fontWeight: FontWeight.bold,
-          ),
-        )
-    );
-
     return MaterialApp(
-      title: 'Parties Application',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Parties demo'),
+        backgroundColor: Colors.green[200],
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Container(
+                color: Colors.red[200],
+                height: 100.0,
+                width: double.infinity,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                color: Colors.red[200],
+                height: 200.00,
+                width: double.infinity,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                color: Colors.red[200],
+                height: 200.0,
+                width: double.infinity,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                color: Colors.red[200],
+                height: 50.0,
+                width: double.infinity,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              )
+            ],
+          ),
         ),
-        body: Column(
-          children: [
-            titleSection,
-            dashboardSection,
-            partiesSection,
-            adSpaceSection
-          ],
-
-        ),
-      ),
+      )
     );
   }
 }
