7a8676c2a831113391417d80a5980136f1004e29
Sam Lightowler <slightowler7@gmail.com>
2020-04-06 17:21:57 +0100

    added dashboard ui

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,15 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(Home());
 
-
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.green[200],
-        body: SafeArea(
-          child: HomePage()
-        ),
-      )
-    );
+        home: Scaffold(
+      backgroundColor: Colors.green[200],
+      body: SafeArea(child: HomePage()),
+    ));
   }
-
 }
 
 class HomePage extends StatefulWidget {
@@ -24,6 +19,41 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int dashBoardTracker = 0;
+
+  // todo: add people tracker
+  // todo: needs moving to own people class
+  List<String> people = [];
+
+  // todo: needs moving to own dashboard class
+  List<String> dashBoardTitle = [
+    'Make your Party!',
+    'Your Profile',
+    'Find Parties',
+    'Settings',
+  ];
+  // todo: needs moving to own dashboard class
+  // todo: add animations
+  // todo: add page navigations
+  // todo: add QR code functionality
+  List<Widget> dashBoard = [
+    Icon(Icons.people, size: 100.0, color: Colors.red[400]),
+    Icon(Icons.account_circle, size: 100.0, color: Colors.red[400]),
+    Icon(Icons.map, size: 100.0, color: Colors.red[400]),
+    Icon(Icons.settings, size: 100.0, color: Colors.red[400]),
+  ];
+
+  void arrowNavigation() {
+    setState(() {
+      if (dashBoardTracker == dashBoard.length - 1) {
+        print('Reached limit');
+        dashBoardTracker = 0;
+      } else {
+        dashBoardTracker++;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -52,8 +82,7 @@ class _HomePageState extends State<HomePage> {
                 style: TextStyle(
                     fontFamily: 'Pacifico',
                     color: Colors.red[400],
-                    fontSize: 50.0
-                ),
+                    fontSize: 50.0),
               )
             ],
           ),
@@ -66,6 +95,50 @@ class _HomePageState extends State<HomePage> {
           height: 200.00,
           width: double.infinity,
           margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                // todo: can possibly be change to IconButton?
+                // todo: add swipe functionality
+                child: FlatButton(
+                  child: Icon(
+                    Icons.arrow_back_ios,
+                    color: Colors.red[400],
+                  ),
+                  onPressed: () {
+                    print('left buttn pressed');
+                    arrowNavigation();
+                  },
+                ),
+              ),
+              Container(
+                width: 200.0,
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      dashBoardTitle[dashBoardTracker],
+                      style: TextStyle(color: Colors.red[400], fontSize: 25.0, fontStyle: FontStyle.italic),
+                    ),
+                    SizedBox(height: 20.0),
+                    dashBoard[dashBoardTracker]
+                  ],
+                ),
+              ),
+              Expanded(
+                // can possibly be change to IconButton?
+                child: FlatButton(
+                  child: Icon(
+                    Icons.arrow_forward_ios,
+                    color: Colors.red[400],
+                  ),
+                  onPressed: () {
+                    print("right button pressed");
+                    arrowNavigation();
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
         SizedBox(
           height: 10.0,
@@ -75,6 +148,29 @@ class _HomePageState extends State<HomePage> {
           height: 200.0,
           width: double.infinity,
           margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+          child: Column(
+            children: <Widget>[
+              // todo: create listview UI
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  FlatButton(
+                      child: Text('Your Party'),
+                      onPressed: () {
+                        print('go to contacts listview');
+                      }),
+                  FlatButton(
+                      child: Text('Parties you\'re in'),
+                      onPressed: () {
+                        print('go to parties listview');
+                      }),
+               Row(
+                    //
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
         SizedBox(
           height: 10.0,
@@ -89,4 +185,3 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
-
